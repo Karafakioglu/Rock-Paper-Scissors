443368955e67a18baa3836983f05491fcb75ed24
Karafakioglu <ismailkarafakioglu@gmail.com>
2020-05-17 12:39:38 +0200

    switch statements to check who won

--- script.js
+++ script.js
@@ -8,16 +8,50 @@ const rock_div = document.getElementById("rock")
 const paper_div = document.getElementById("paper")
 const scissors_div = document.getElementById("scissors")
 
-function game(userChoice) {
-	alert(userChoice);
-}
-
 // Computer chooses randomly out of defined const array
 
-function compChoice(){
+function getCompChoice(){
 	const choices = ['rock','paper','scissors'];
 	let choice = choices[Math.floor(Math.random()*choices.length)];
 	return choice;
+}
+
+// Game Logic
+
+function game(userChoice) {
+	const compChoice= getCompChoice();
+
+	console.log(`----- user choise is ${userChoice} and computer choice is ${compChoice} -----`);
+
+	// Checks whether user wins or not
+
+	switch (userChoice+compChoice) {
+		case "rockscissors":
+		case "paperrock":
+		case "scissorspaper":
+		console.log("user wins!");
+		break;
+	}
+
+	// Checks whether comp wins or not
+
+	switch (compChoice+userChoice) {
+		case "rockscissors":
+		case "paperrock":
+		case "scissorspaper":
+		console.log("computer wins!");
+		break;
+	}
+
+	// Checks whether it is a tie or not
+
+	switch(compChoice+userChoice){
+		case "rockrock":
+		case "paperpaper":
+		case "scissorsscissors":
+		console.log("It is a tie!");
+		break;
+	}
 }
 
 // Main
@@ -35,7 +69,7 @@ function main() {
 		game('scissors');
 	})
 
-	console.log(compChoice());
+
 }
 
 main();
