554cf7b136ed4be8df2c156edc47553df3955084
Karafakioglu <ismailkarafakioglu@gmail.com>
2023-05-18 00:33:08 +0200

    add computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello!")
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random()*3);
+    var computerChoice;
+
+
+    switch (randomNumber) {
+        case 0:
+            computerChoice = "Rock"
+            break;
+        case 1:
+            computerChoice = "Paper"
+            break;
+        case 2:
+            computerChoice = "Scissors"
+            break;
+        default:
+            console.log("Something aint right!")
+    }
+
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
