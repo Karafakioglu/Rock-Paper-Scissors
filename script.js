f64eeb63c8c79498ece246f9379fe28217ff7faf
Karafakioglu <ismailkarafakioglu@gmail.com>
2023-05-18 23:21:28 +0200

    implement the logic that keeps track of the winner

--- script.js
+++ script.js
@@ -24,37 +24,57 @@ function getUserChoice() {
 
 function playGame(computer, user) {
     let output;
+    let number = 0;
 
     if(computer === "rock" && user === "paper"){
         output = `PC chose ${computer} and user chose ${user}. User wins!`
+        number++;
     }
     else if(computer === "rock" && user === "scissors"){
         output = `PC chose ${computer} and user chose ${user}. PC wins!`
+        number--;
     }
     else if (computer === "paper" && user === "rock"){
         output = `PC chose ${computer} and user chose ${user}. PC wins!`
+        number--;
     }
     else if (computer === "paper" && user === "scissors"){
         output = `PC chose ${computer} and user chose ${user}. User wins!`
+        number++;
     }
     else if (computer === "scissors" && user === "rock"){
         output = `PC chose ${computer} and user chose ${user}. User wins!`
+        number++;
     }
     else if (computer === "scissors" && user === "paper"){
         output = `PC chose ${computer} and user chose ${user}. PC wins!`
+        number--;
     }
     else{
         output = `PC chose ${computer} and user chose ${user}. It is a tie!`
     }
-    return output;
+    return {output,number};
 }
 
 function game(){
     let gameCounter = 5;
+    let userScore = 0;
     while(gameCounter){
+
     let test = playGame(getComputerChoice(),getUserChoice());
-    console.log(test);
+    console.log(test.output);
+    userScore += test.number;
     gameCounter--;
+
+    }
+    if(userScore < 0){
+        console.log("Pc wins the game!")
+    }
+    else if (userScore > 0){
+        console.log("user wins the game!")
+    }
+    else{
+        console.log("It is a tie game!")
     }
 }
 
