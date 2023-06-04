d30e1cc13ebc6482e6e3a00140a7936dceec0d6a
Karafakioglu <ismailkarafakioglu@gmail.com>
2023-06-04 21:59:22 +0200

    show user/comp score when button clicked

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
- function getComputerChoice() {
-     let randomNumber = Math.floor(Math.random() * 3);
-     var computerChoice;
-     switch (randomNumber) {
-         case 0:
-             computerChoice = "rock"
-             break;
-         case 1:
-             computerChoice = "paper"
-             break;
-         case 2:
-             computerChoice = "scissors"
-             break;
-         default:
-             console.log("Something aint right!")
-     }
-     return computerChoice;
- }
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    var computerChoice;
+    switch (randomNumber) {
+        case 0:
+            computerChoice = "rock"
+            break;
+        case 1:
+            computerChoice = "paper"
+            break;
+        case 2:
+            computerChoice = "scissors"
+            break;
+        default:
+            console.log("Something aint right!")
+    }
+    return computerChoice;
+}
 
 // function getUserChoice() {
 //     let userChoice = prompt("Please choose: rock || paper || scissors").toLowerCase();
@@ -24,61 +24,62 @@
 
 function playGame(computer, user) {
     let output;
-    let roundScore = 0;
+    let userScore = 0;
+    let computerScore = 0;
 
-    if(computer === "rock" && user === "paper"){
+    if (computer === "rock" && user === "paper") {
         output = `PC chose ${computer} and user chose ${user}. User wins!`
-        roundScore++;
+        userScore++;
     }
-    else if(computer === "rock" && user === "scissors"){
+    else if (computer === "rock" && user === "scissors") {
         output = `PC chose ${computer} and user chose ${user}. PC wins!`
-        roundScore--;
+        computerScore++;
     }
-    else if (computer === "paper" && user === "rock"){
+    else if (computer === "paper" && user === "rock") {
         output = `PC chose ${computer} and user chose ${user}. PC wins!`
-        roundScore--;
+        computerScore++;
     }
-    else if (computer === "paper" && user === "scissors"){
+    else if (computer === "paper" && user === "scissors") {
         output = `PC chose ${computer} and user chose ${user}. User wins!`
-        roundScore++;
+        userScore++;
     }
-    else if (computer === "scissors" && user === "rock"){
+    else if (computer === "scissors" && user === "rock") {
         output = `PC chose ${computer} and user chose ${user}. User wins!`
-        roundScore++;
+        userScore++;
     }
-    else if (computer === "scissors" && user === "paper"){
+    else if (computer === "scissors" && user === "paper") {
         output = `PC chose ${computer} and user chose ${user}. PC wins!`
-        roundScore--;
+        computerScore++;
     }
-    else{
+    else {
         output = `PC chose ${computer} and user chose ${user}. It is a tie!`
     }
-    return {output,roundScore};
+    return { output, userScore, computerScore };
 }
 
-function game(){
-    let gameCounter = 5;
-    let userScore = 0;
-    while(gameCounter){
+// function game(){
+//     let gameCounter = 5;
+//     let userScore = 0;
+//     while(gameCounter){
 
-    let roundResult = playGame(getComputerChoice(),getUserChoice());
-    console.log(roundResult.output);
-    // userScore += roundResult.roundScore;
-    // gameCounter--;
+//     let roundResult = playGame(getComputerChoice(),getUserChoice());
+//     console.log(roundResult.output);
+//     // userScore += roundResult.roundScore;
+//     // gameCounter--;
 
-    }
-    // if(userScore < 0){
-    //     console.log("Pc wins the game!")
-    // }
-    // else if (userScore > 0){
-    //     console.log("user wins the game!")
-    // }
-    // else{
-    //     console.log("It is a tie game!")
-    // }
-}
+//     }
+//     // if(userScore < 0){
+//     //     console.log("Pc wins the game!")
+//     // }
+//     // else if (userScore > 0){
+//     //     console.log("user wins the game!")
+//     // }
+//     // else{
+//     //     console.log("It is a tie game!")
+//     // }
+// }
 
-// game();
+// // game();
 
 const buttons = document.querySelectorAll('.button')
 const result = document.querySelector('#result')
@@ -89,21 +90,23 @@ result.innerText = "Result placeholder"
 userScoreElement.innerText = 0;
 computerScoreElement.innerText = 0;
 
-let gameCounter = 5;
-let userScore = 0;
-let computerScore = 0;
-
-buttons.forEach(button => button.addEventListener('click',e => {
+buttons.forEach(button => button.addEventListener('click', e => {
 
     let computer = getComputerChoice();
     let user = e.target.innerText.toLowerCase();
-    let roundResult = playGame(computer,user);
-    // console.log(roundResult.output)
-    gameCounter--;
+    let roundResult = playGame(computer, user);
+    let userPlusOne = roundResult.userScore;
+    let computerPlusOne = roundResult.computerScore;
+
+
+
+
 
     result.innerText = roundResult.output
-    userScoreElement.innerText = userScore;
-    computerScoreElement.innerText = computerScore;
+    userScoreElement.innerText = userPlusOne + parseInt(userScoreElement.innerText);
+    computerScoreElement.innerText = computerPlusOne + parseInt(computerScoreElement.innerText);
+
+
 }))
 
 
